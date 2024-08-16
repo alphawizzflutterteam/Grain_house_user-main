5a74a36fc2d5da64a11294299fe1369de2abfe6c
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-16 15:03:39 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,20 @@
+final String appName = 'GRAIN HOUSE';
+
+final String packageName = 'com.grainhouse.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.grainhouse.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.grainhouse.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://developmentalphawizz.com/grain_house/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/grain_house/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/burans/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
